277a23916ff6e6a1aed2f50d27023f403d0c6a17
Brian Gann <bkgann@gmail.com>
2019-12-20 10:05:16 -0600

    remove gometalinter

--- build/circle-cmd-lint.sh
+++ build/circle-cmd-lint.sh
@@ -11,21 +11,10 @@ function exit_if_fail {
   fi
 }
 
-# gometalinter needs newer version of go-i18n
-#go get -u github.com/nicksnyder/go-i18n/v2/i18n
-go get -u github.com/nicksnyder/go-i18n
-go get -u github.com/alecthomas/gometalinter
 go get -u github.com/jgautheron/goconst/cmd/goconst
 go get -u honnef.co/go/tools/cmd/staticcheck
 go get -u github.com/mgechev/revive
 go get -u github.com/golangci/golangci-lint/cmd/golangci-lint
-
-# use gometalinter when lints are not available in golangci or
-# when gometalinter is better. Eg. goconst for gometalinter does not lint test files
-# which is not desired.
-exit_if_fail gometalinter --enable-gc --vendor --deadline 10m --disable-all \
-  --enable=goconst\
-  --enable=staticcheck
 
 # use golangci-when possible
 exit_if_fail golangci-lint run --deadline 10m --disable-all \

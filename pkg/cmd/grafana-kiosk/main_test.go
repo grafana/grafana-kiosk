3494600945c0ac8e438bb71c1e0a4d201e8311b9
Brian Gann <bkgann@gmail.com>
2024-03-08 23:46:54 -0500

    add autofit tests

--- pkg/cmd/grafana-kiosk/main_test.go
+++ pkg/cmd/grafana-kiosk/main_test.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"log"
 	"os"
 	"testing"
 
 	"github.com/grafana/grafana-kiosk/pkg/kiosk"
+	"github.com/ilyakaznacheev/cleanenv"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
@@ -46,6 +48,36 @@ func TestMain(t *testing.T) {
 				APIKey: "abc",
 			},
 		}
+		Convey("General Options", func() {
+			Convey("Parameter - autofit", func() {
+				oldArgs := os.Args
+				defer func() { os.Args = oldArgs }()
+				os.Args = []string{"grafana-kiosk", ""}
+				// starts out default true
+				result := ProcessArgs(cfg)
+				So(result.AutoFit, ShouldBeTrue)
+				// flag to set it false
+				os.Args = []string{
+					"grafana-kiosk",
+					"--autofit=false",
+				}
+				result = ProcessArgs(cfg)
+				So(result.AutoFit, ShouldBeFalse)
+			})
+
+			Convey("Environment - autofit", func() {
+				oldArgs := os.Args
+				defer func() { os.Args = oldArgs }()
+				os.Args = []string{"grafana-kiosk", ""}
+				os.Setenv("KIOSK_AUTOFIT", "false")
+				cfg := kiosk.Config{}
+				if err := cleanenv.ReadEnv(&cfg); err != nil {
+					log.Println("Error reading config from environment", err)
+				}
+				So(cfg.General.AutoFit, ShouldBeFalse)
+			})
+		})
+		// end of general options
 
 		Convey("Anonymous Login", func() {
 			oldArgs := os.Args
@@ -54,6 +86,7 @@ func TestMain(t *testing.T) {
 			result := ProcessArgs(cfg)
 			So(result.LoginMethod, ShouldEqual, "anon")
 			So(result.URL, ShouldEqual, "https://play.grafana.org")
+			So(result.AutoFit, ShouldBeTrue)
 		})
 		Convey("Local Login", func() {
 			oldArgs := os.Args
@@ -62,6 +95,7 @@ func TestMain(t *testing.T) {
 			result := ProcessArgs(cfg)
 			So(result.LoginMethod, ShouldEqual, "local")
 			So(result.URL, ShouldEqual, "https://play.grafana.org")
+			So(result.AutoFit, ShouldBeTrue)
 		})
 	})
 }

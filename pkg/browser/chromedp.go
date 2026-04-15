2414798186b4efb58639b3fbbf952f3d68e3fc7e
Brian Gann <bkgann@gmail.com>
2026-04-15 02:04:31 -0400

    feat: add ChromeDP implementation of browser.Browser

---
+++ pkg/browser/chromedp.go
@@ -0,0 +1,26 @@
+package browser
+
+import (
+	"context"
+
+	"github.com/chromedp/chromedp"
+)
+
+// ChromeDP implements Browser by delegating to chromedp.
+type ChromeDP struct{}
+
+func (c *ChromeDP) Navigate(ctx context.Context, url string) error {
+	return chromedp.Run(ctx, chromedp.Navigate(url))
+}
+
+func (c *ChromeDP) WaitVisible(ctx context.Context, sel string) error {
+	return chromedp.Run(ctx, chromedp.WaitVisible(sel, chromedp.BySearch))
+}
+
+func (c *ChromeDP) Click(ctx context.Context, sel string) error {
+	return chromedp.Run(ctx, chromedp.Click(sel, chromedp.BySearch))
+}
+
+func (c *ChromeDP) SendKeys(ctx context.Context, sel string, value string) error {
+	return chromedp.Run(ctx, chromedp.SendKeys(sel, value, chromedp.BySearch))
+}
